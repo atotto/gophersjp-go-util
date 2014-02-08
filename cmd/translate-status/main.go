12ab197b98e6d190037d2469be9d28c049816e45
Ato Araki <ato.araki@gmail.com>
2014-02-08 22:12:24 +0900

    add other repos

--- cmd/translate-status/main.go
+++ cmd/translate-status/main.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 	"strings"
 
+	"go/build"
 	"github.com/atotto/gophersjp-go-util/hg"
 	"github.com/atotto/gophersjp-go-util/translated"
 )
@@ -23,24 +24,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	repos, err := hg.AttachRepos(runtime.GOROOT())
+	goRepos, err := hg.AttachRepos(runtime.GOROOT())
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	p, err := build.Import("code.google.com/p/go.tools", build.Default.GOROOT, build.FindOnly)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gotoolRepos, err := hg.AttachRepos(p.Dir)
 
 	d := NewData()
 	for _, path := range list {
 		log.Printf("== %s\n", path)
 
-		if strings.HasPrefix(path, "src/pkg/code.google.com") {
-			log.Println("hoge")
-		}
-
 		rev, err := tr.GetRevision(*root + "/" + path)
 		if err != nil {
 			log.Fatal("rev:" + err.Error())
 		}
-		b, err := repos.Diff(path, rev.String())
+		var b []byte
+		if strings.HasPrefix(path, "src/pkg/code.google.com") {
+			b, err = gotoolRepos.Diff(path, rev.String())
+		} else {
+			b, err = goRepos.Diff(path, rev.String())
+		}
 		var revision = "LATEST"
 		if len(b) != 0 {
 			revision = rev.String()
