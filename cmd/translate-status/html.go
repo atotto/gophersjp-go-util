1a4b1f368cba752e4d867cbc165afbd62dba9fb5
Ato ARAKI <ato.araki@gmail.com>
2014-04-27 04:29:39 +0900

    add target="_blank"

--- cmd/translate-status/html.go
+++ cmd/translate-status/html.go
@@ -112,11 +112,11 @@ const tmplHTML = `
         border-radius: 4px;
       }
   </style>
-  <h3><a href="https://code.google.com/p/go">Go</a>翻訳ステータス</h3>
+  <h3><a href="https://code.google.com/p/go" target="_blank">Go</a>翻訳ステータス</h3>
   <p>
     翻訳したドキュメントが最新のものかどうかをチェックしています。
     なお、本サイトは github.com/gophersjp/go の成果物です。ご気軽にご参加ください。
-    翻訳活動に参加していただける方は、<a href="https://github.com/gophersjp/go/wiki/%E7%BF%BB%E8%A8%B3%E5%8D%94%E5%8A%9B%E3%83%95%E3%83%AD%E3%83%BC">翻訳協力フロー</a>を一読の後、<a href="#non_translated">未翻訳リスト</a>からissueを登録ください。
+    翻訳活動に参加していただける方は、<a href="https://github.com/gophersjp/go/wiki/%E7%BF%BB%E8%A8%B3%E5%8D%94%E5%8A%9B%E3%83%95%E3%83%AD%E3%83%BC" target="_blank">翻訳協力フロー</a>を一読の後、<a href="#non_translated">未翻訳リスト</a>からissueを登録ください。
   </p>
   <p>
   <a href="https://github.com/gophersjp/go" class="download" id="start" target="_blank">
@@ -130,26 +130,26 @@ const tmplHTML = `
     <colgroup class="col-issues"></colgroup>
     <tbody>
       <tr>
-        <th><a href="https://github.com/gophersjp/go">translated item<a></th>
-        <th><a href="https://code.google.com/p/go/source/browse">{{.Tag}}</a></th>
-        <th><a href="https://code.google.com/p/go/source/browse">tip</a></th>
-        <th colspan="2"><a href="https://github.com/gophersjp/go/issues?state=open">issues</a></th>
+        <th><a href="https://github.com/gophersjp/go" target="_blank">translated item<a></th>
+        <th><a href="https://code.google.com/p/go/source/browse" target="_blank">{{.Tag}}</a></th>
+        <th><a href="https://code.google.com/p/go/source/browse" target="_blank">tip</a></th>
+        <th colspan="2"><a href="https://github.com/gophersjp/go/issues?state=open" target="_blank">issues</a></th>
       </tr>
       {{range $i, $f := .Items}}
       <tr class="item">
         <td><a href="https://github.com/gophersjp/go/blob/master/{{$f.FilePath}}">{{$f.FilePath}}</a></td>
         <td>
           {{if $f.Stable.IsOutdated}}
-            <a href="{{$f.RepoURL}}" class="outdated">{{$f.Stable.Stage}}</a>
+            <a href="{{$f.RepoURL}}" class="outdated" target="_blank">{{$f.Stable.Stage}}</a>
           {{else}}
-            <a href="{{$f.RepoURL}}" class="latest">{{$f.Stable.Stage}}</a>
+            <a href="{{$f.RepoURL}}" class="latest" target="_blank">{{$f.Stable.Stage}}</a>
           {{end}}
         </td>
         <td>
           {{if $f.Tip.IsOutdated}}
-            <a href="{{$f.RepoURL}}" class="latest">{{$f.Tip.Stage}}</a>
+            <a href="{{$f.RepoURL}}" class="latest" target="_blank">{{$f.Tip.Stage}}</a>
           {{else}}
-            <a href="{{$f.RepoURL}}" class="latest">{{$f.Tip.Stage}}</a>
+            <a href="{{$f.RepoURL}}" class="latest" target="_blank">{{$f.Tip.Stage}}</a>
           {{end}}
         </td>
         <td>
@@ -169,12 +169,12 @@ const tmplHTML = `
     <colgroup class="col-issues"></colgroup>
     <tbody>
       <tr>
-        <th><a href="https://github.com/gophersjp/go">non-translated item<a></th>
+        <th><a href="https://github.com/gophersjp/go" target="_blank">non-translated item<a></th>
         <th colspan="2">翻訳用チケット</a></th>
       </tr>
       {{range $i, $f := .NonTranslatedItems}}
       <tr class="item">
-        <td><a href="{{$f.RepoURL}}">{{$f.FilePath}}</a></td>
+        <td><a href="{{$f.RepoURL}}" target="_blank">{{$f.FilePath}}</a></td>
         <td>
           <a href="https://github.com/gophersjp/go/search?type=Issues&q={{$f.KeyName}}" target="_blank">検索</a>
         </td>
